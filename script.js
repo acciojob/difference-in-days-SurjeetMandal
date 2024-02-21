ecc353f91d6f86a466d7155c80eb729152b107d7
SurjeetMandal <surjeetmandal698@gmail.com>
2024-02-21 10:53:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 var dateDiffInDays = function (date1, date2) {
-  //   write your code here
+	let time = Math.abs(date1 - date2);
+	let days = Math.ceil(time / (1000 * 60 * 60 * 24))
+	return days;
 };
 
 // Do not change the code below.
 const dateOne = prompt("Enter Start Date.");
 const dateTwo = prompt("Enter End Date.");
 alert(dateDiffInDays(dateOne, dateTwo));
+
+
